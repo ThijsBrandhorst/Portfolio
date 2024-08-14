9966f1763f2bd0d5514cf46a93c35c9905b137da
Thijs <thijsbrandhorst007@gmail.com>
2024-08-14 13:14:17 +0200

    MailingFix

---
+++ js/config.js
@@ -0,0 +1,9 @@
+//Config for EmailJS
+
+const config = {
+    emailJsUserId: 'S2NbsUtMtDe4HFHoS',
+    emailJsServiceId: 'service_8dnwopg',
+    emailJsTemplateId: 'template_u0ll8vi'
+};
+
+export default config;
